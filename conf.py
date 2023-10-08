cbaee079c8e6af1a234b2f9de317fd5a70d8d328
CreativeJr <73512563+CreativeJr@users.noreply.github.com>
2023-10-08 20:27:01 +0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5535847652:AAFkR7zNSByPnkKx_nw0chq5zT95O1_esOk")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 4926633))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "be7b1d39ac5116d22701f8b6ac956785")
 
